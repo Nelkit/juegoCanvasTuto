571e134f5ec293c2ff904ae8785271b06d578611
Nelkit <nelkitisael792@gmail.com>
2014-08-30 22:27:00 -0600

    Agregando a Tifis y a Botones para movimiento

--- tifis.js
+++ tifis.js
@@ -1,7 +1,15 @@
 var tablero;
 var fondo = {
 	imagenURL: "fondo.png",
-}
+	imagenOK: false
+};
+
+var tifis = {
+	x: 100,
+	y: 100,
+	frenteURL: "diana-frente.png",
+	frenteOK: false
+};
 
 function inicio () 
 {
@@ -9,11 +17,71 @@ function inicio ()
 	tablero = canvas.getContext("2d");
 	fondo.imagen = new Image();
 	fondo.imagen.src = fondo.imagenURL;
+	fondo.imagen.onload = confirmarFondo;
 
-	fondo.imagen.onload = dibujarFondo;
+	tifis.frente = new Image();
+	tifis.frente.src = tifis.frenteURL;
+	tifis.frente.onload = confirmarFrente;
+
+	var d,i,ar,ab;
+	d = document.getElementById("moverDer");
+	d.addEventListener('click', moverTifisDer);
+
+	i = document.getElementById("moverIzq");
+	i.addEventListener('click', moverTifisIzq);
+
+	ar = document.getElementById("moverArr");
+	ar.addEventListener('click', moverTifisArr);
+
+	ab = document.getElementById("moverAba");
+	ab.addEventListener('click', moverTifisAba);
 }
 
-function dibujarFondo () 
+function confirmarFondo()
 {
-	tablero.drawImage(fondo.imagen, 0, 0);	
+	fondo.imagenOK = true;
+	dibujar();
+}
+
+function confirmarFrente()
+{
+	tifis.frenteOK = true;
+	dibujar();
+}
+
+function dibujar () 
+{
+	if (fondo.imagenOK == true) 
+	{
+		tablero.drawImage(fondo.imagen, 0, 0);
+	};
+
+	if (tifis.frenteOK == true) 
+	{
+		tablero.drawImage(tifis.frente, tifis.x, tifis.y);
+	};
+}
+
+function moverTifisDer()
+{
+	tifis.x += 10;
+	dibujar(); 
+}
+
+function moverTifisIzq()
+{
+	tifis.x -= 10;
+	dibujar(); 
+}
+
+function moverTifisArr()
+{
+	tifis.y += 10;
+	dibujar(); 
+}
+
+function moverTifisAba()
+{
+	tifis.y -= 10;
+	dibujar(); 
 }
